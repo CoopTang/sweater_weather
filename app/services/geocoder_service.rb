caddca47ce7a910bef70f517c774fe245c3b5d8c
Michael Cooper <macoop2363@gmail.com>
2020-01-13 10:49:43 -0700

    Add new Geocoding method to get the cities of a given lat/long and its respective test

--- app/services/geocoder_service.rb
+++ app/services/geocoder_service.rb
@@ -1,19 +1,30 @@
 class GeocoderService
 
+  GOOGLE_API_URL = 'https://maps.googleapis.com/maps/api'.freeze
+  GOOGLE_GEOCODE_PATH = 'geocode/json'.freeze
+
   def self.get_geocoding(location)
     response = ApiQuerier.query_api(
-      'https://maps.googleapis.com/maps/api',
-      'geocode/json',
-      create_params(location)
+      GOOGLE_API_URL,
+      GOOGLE_GEOCODE_PATH,
+      create_params('address', location)
+    )
+  end
+
+  def self.get_reverse_geocoding(location)
+    response = ApiQuerier.query_api(
+      GOOGLE_API_URL,
+      GOOGLE_GEOCODE_PATH,
+      create_params('latlng', "#{location.latitude},#{location.longitude}")
     )
   end
 
   private_class_method
 
-  def self.create_params(location)
+  def self.create_params(key, location)
     {
       params: {
-        'address' => location,
+        key => location,
         'key' => ENV['GOOGLE_GEOCODE_API_KEY']
       }
     }
--- spec/services/geocoder_service_spec.rb
+++ spec/services/geocoder_service_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe GeocoderService do
     expect(response[:results][0][:geometry][:location]).to have_key(:lat)
     expect(response[:results][0][:geometry][:location]).to have_key(:lng)
   end
+
+  it 'can get the city(s) at a lat long location', :vcr do
+    latlng = GeoCoordinate.new({ lat: -22.3193039, lng: -65.8306389 })
+
+    response = GeocoderService.get_reverse_geocoding(latlng)
+
+    expect(response).to have_key(:results)
+    expect(response[:results]).to be_an(Array)
+    expect(response[:results].first).to have_key(:formatted_address)
+  end
 end
