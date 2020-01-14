cd135659d5d5f7f515f86130dce3982ea79790a6
Michael Cooper <macoop2363@gmail.com>
2020-01-14 13:21:35 -0700

    Rename method to accurately reflect the API response

--- app/services/giphy_service.rb
+++ app/services/giphy_service.rb
@@ -2,7 +2,7 @@ class GiphyService
   GIPHY_API_URL = 'https://api.giphy.com/v1'.freeze
   GIPHY_GIF_SEARCH_API = 'gifs/search'
 
-  def self.get_gif(term)
+  def self.search_gifs(term)
     response = ApiQuerier.query_api(
       GIPHY_API_URL,
       GIPHY_GIF_SEARCH_API,
--- spec/services/giphy_service_spec.rb
+++ spec/services/giphy_service_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe GiphyService do
   it 'Can get weather of a given latitude and longitude location', :vcr do
     term = 'Mostly Cloudy'
 
-    response = GiphyService.get_gif(term)
+    response = GiphyService.search_gifs(term)
 
     expect(response).to have_key(:data)
     expect(response[:data][0][:url]).to eq("https://giphy.com/gifs/sky-clouds-cloud-HoUgegTjteXCw")
