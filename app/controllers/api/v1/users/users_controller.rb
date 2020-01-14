56b4ac980eed69cd597b5342b9e9753b27803a26
Michael Cooper <macoop2363@gmail.com>
2020-01-14 12:26:17 -0700

    Add invalid user creation path and a before_action to set the headers for the response

--- app/controllers/api/v1/users/users_controller.rb
+++ app/controllers/api/v1/users/users_controller.rb
@@ -1,22 +1,29 @@
 require 'securerandom'
 class Api::V1::Users::UsersController < ApplicationController
+  before_action :set_headers
 
   def create
     user = User.new(user_params)
     if user.save
-      render json: { api_key: user.api_key}
+      render json: { api_key: user.api_key }
     else
+      render json: { message: user.errors.full_messages.to_sentence }
     end
   end
-
+  
   private
-
+  
   def user_params
     { api_key: generate_api_key}
-      .merge(params.permit(:email, :password, :password_confirmation))
+    .merge(params.permit(:email, :password, :password_confirmation))
   end
-
+  
   def generate_api_key
     SecureRandom.urlsafe_base64
+  end
+  
+  def set_headers
+    response.set_header('Content-Type', 'application/json')
+    response.set_header('Accept', 'application/json')
   end
 end
