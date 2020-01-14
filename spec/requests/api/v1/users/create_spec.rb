9ed8b4af4c77feae74d8bb61a3d9dc3337e62950
Michael Cooper <macoop2363@gmail.com>
2020-01-14 12:25:49 -0700

    Add tests for invalid user credentials

--- spec/requests/api/v1/users/create_spec.rb
+++ spec/requests/api/v1/users/create_spec.rb
@@ -11,10 +11,29 @@ describe 'User Create:', type: :request do
         password: 'password',
         password_confirmation: 'password'
       }
-
+      
       response = JSON.parse(@response.body, symbolize_names: true)
-
+      
       expect(response[:api_key]).to eq(api_key)
+    end
+
+    it 'Sends back a message if there are valid credentials' do
+      User.create(
+        email: 'bob@email.com',
+        password: 'password',
+        password_confirmation: 'password',
+        api_key: 'asdf'
+      )
+
+      post '/api/v1/users', params: {
+        email: 'bob@email.com',
+        password: 'password',
+        password_confirmation: 'not'
+      }
+      
+      response = JSON.parse(@response.body, symbolize_names: true)
+      
+      expect(response[:message]).to eq("Password confirmation doesn't match Password and Email has already been taken")
     end
   end
 end
