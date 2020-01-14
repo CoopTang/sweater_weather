9b169f960ad9203874053c5693c920a2d8291598
Michael Cooper <macoop2363@gmail.com>
2020-01-14 11:16:53 -0700

    Update to schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 2020_01_14_175855) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
