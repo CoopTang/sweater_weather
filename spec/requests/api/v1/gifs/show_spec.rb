47e8d54f87faac5045fd62514c3b47350a17e53a
Michael Cooper <macoop2363@gmail.com>
2020-01-14 14:22:57 -0700

    Update tests to reflect new response

--- spec/requests/api/v1/gifs/show_spec.rb
+++ spec/requests/api/v1/gifs/show_spec.rb
@@ -5,49 +5,16 @@ describe 'Gifs Show:' do
     it '; It returns a response with the following parameters', :vcr do
       get '/api/v1/gifs?location=denver,co'
 
-      response = JSON.parse(@response.body)
+      response = JSON.parse(@response.body, symbolize_names: true)
 
       expect(response).to have_key(:data)
-      expect(response).to have_key(:copyright)
-      expect(response[:copyright]).to eq('2019')
-      expect(response[:data]).to have_key(:images)
-      expect(response[:data][:images]).to be_an(Array)
-      expect(response[:data][:images][0]).to have_key(:time)
-      expect(response[:data][:images][0]).to have_key(:url)
-      expect(response[:data][:images][0]).to have_key(:summary)
+      expect(response[:data][:attributes]).to have_key(:copyright)
+      expect(response[:data][:attributes][:copyright]).to eq('2019')
+      expect(response[:data][:attributes]).to have_key(:images)
+      expect(response[:data][:attributes][:images]).to be_an(Array)
+      expect(response[:data][:attributes][:images][0]).to have_key(:time)
+      expect(response[:data][:attributes][:images][0]).to have_key(:url)
+      expect(response[:data][:attributes][:images][0]).to have_key(:summary)
     end
   end
 end
-
-# {
-#   data: {
-#     images: [
-#       {
-#         time: "1541487600",
-#         summary: "Mostly sunny in the morning.",
-#         url: "<GIPHY_URL_GOES_HERE>"
-#       },
-#       {
-#         time: "1541127600",
-#         summary: "Partly cloudy in the evening.",
-#         url: "<GIPHY_URL_GOES_HERE>"
-#       },
-#       {
-#         time: "2381487600",
-#         summary: "Snowy.",
-#         url: "<GIPHY_URL_GOES_HERE>"
-#       },
-#       {
-#         time: "1541487600",
-#         summary: "Firenado",
-#         url: "<GIPHY_URL_GOES_HERE>"
-#       },
-#       {
-#         time: "1541487600",
-#         summary: "Corgis because I cant think of other weather",
-#         url: "<GIPHY_URL_GOES_HERE>"
-#       }
-#     ]     
-#   },
-#   copyright: "2019"
-# }
