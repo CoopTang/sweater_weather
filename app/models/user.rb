90333c2d12eb2ab45c1372efe79b7b80bac2270f
Michael Cooper <macoop2363@gmail.com>
2020-01-14 11:03:57 -0700

    Add User model and its respective validation tests

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  has_secure_password
+
+  validates_presence_of :name
+  validates_presence_of :email
+  validates_uniqueness_of :email
+  validates_presence_of :password_digest
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password) }
+  end
+
+  describe 'relationships' do
+  end
+end
