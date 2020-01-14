cd568a38bfbd3439e01361efd0b97659d1b88f11
Michael Cooper <macoop2363@gmail.com>
2020-01-14 10:02:26 -0700

    Rename file and moved it to facade folder

---
+++ app/facades/forecast_facade.rb
@@ -0,0 +1,57 @@
+class Forecast
+  attr_reader :timezone,
+              :currently,
+              :hourly,
+              :daily,
+              :id
+
+  def initialize(location)
+    @id        = 1
+    @timezone  = ""
+    @currently = {}
+    @hourly    = []
+    @daily     = []
+    get_forecast(location)
+  end
+
+  def get_forecast(location)
+    coordinate = get_geocode_coordinate(location)
+    parse_forecast(DarkSkyService.get_weather(coordinate))
+  end
+
+  private
+
+  def get_geocode_coordinate(location)
+    geo_code = GeocoderService.get_geocoding(location)[:results][0][:geometry]
+    GeoCoordinate.new(geo_code[:location])
+  end
+
+  def parse_forecast(forecast)
+    @timezone = forecast[:timezone]
+    @currently = Currently.new(forecast[:currently])
+    forecast[:hourly][:data].each do |hour|
+      @hourly.push(parse_hourly(hour))
+    end
+    forecast[:daily][:data].each do |day|
+      @daily.push(parse_daily(day))
+    end
+  end
+
+  def parse_hourly(hourly)
+    hourly.slice(
+      :time,
+      :summary,
+      :icon,
+      :temperature
+    )
+  end
+
+  def parse_daily(daily)
+    daily.slice(
+      :temperatureLow,
+      :temperatureHigh,
+      :precipProbability,
+      :precipType
+    )
+  end
+end
