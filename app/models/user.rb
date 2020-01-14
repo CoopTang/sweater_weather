41862742c8e3250267f7757812a0bee8dd602a69
Michael Cooper <macoop2363@gmail.com>
2020-01-14 11:11:19 -0700

    Remove name from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates_presence_of :name
   validates_presence_of :email
   validates_uniqueness_of :email
   validates_presence_of :password_digest
