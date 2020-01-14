f053fb1a060f22656e26ddccb3e785e7d92fc2ce
Michael Cooper <macoop2363@gmail.com>
2020-01-14 13:57:58 -0700

    Add GifFacade to display on the gifs API endpoint

---
+++ app/facades/gif_facade.rb
@@ -0,0 +1,34 @@
+class GifFacade 
+  attr_reader :images
+
+  def initialize(location)
+    @id = 1
+    @images = []
+    get_images(location)
+  end
+
+  private
+
+  def get_images(location)
+    coordinate = get_geocode_coordinate(location)
+    parse_forecast(DarkSkyService.get_weather(coordinate))
+  end
+
+  def get_geocode_coordinate(location)
+    geo_code = GeocoderService.get_geocoding(location)[:results][0][:geometry]
+    GeoCoordinate.new(geo_code[:location])
+  end
+
+  def parse_forecast(forecast)
+    create_images(forecast[:hourly])
+  end
+
+  def create_images(hourly_forecast)
+    hourly_forecast[:data].each do |hour_data|
+      summary = hour_data[:summary]
+      time    = hour_data[:time]
+      gif_url = GiphyService.get_gif(hour_data[:summary]).first[:url]
+      images.push({ time: time, summary: summary, url: gif_url })
+    end
+  end
+end
