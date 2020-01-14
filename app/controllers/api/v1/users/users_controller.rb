5273ad835fb08ab1eb248e6745416081af114ff6
Michael Cooper <macoop2363@gmail.com>
2020-01-14 12:09:52 -0700

    Add user creation to controller action

--- app/controllers/api/v1/users/users_controller.rb
+++ app/controllers/api/v1/users/users_controller.rb
@@ -1,5 +1,22 @@
+require 'securerandom'
 class Api::V1::Users::UsersController < ApplicationController
 
   def create
+    user = User.new(user_params)
+    if user.save
+      render json: { api_key: user.api_key}
+    else
+    end
+  end
+
+  private
+
+  def user_params
+    { api_key: generate_api_key}
+      .merge(params.permit(:email, :password, :password_confirmation))
+  end
+
+  def generate_api_key
+    SecureRandom.urlsafe_base64
   end
 end
