ba5b70653f59f5cafe712b9a344bd8e25c63b9a1
Michael Cooper <macoop2363@gmail.com>
2020-01-14 13:20:45 -0700

    Add GiphyService to query the Giphy API and search for a gif

---
+++ app/services/giphy_service.rb
@@ -0,0 +1,23 @@
+class GiphyService
+  GIPHY_API_URL = 'https://api.giphy.com/v1'.freeze
+  GIPHY_GIF_SEARCH_API = 'gifs/search'
+
+  def self.get_gif(term)
+    response = ApiQuerier.query_api(
+      GIPHY_API_URL,
+      GIPHY_GIF_SEARCH_API,
+      create_params(term)
+    )
+  end
+
+  private_class_method
+
+  def self.create_params(term)
+    {
+      params: {
+        'q' => term,
+        'key' => ENV['GIPHY_API_KEY']
+      }
+    }
+  end
+end
---
+++ spec/services/giphy_service_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe GiphyService do
+  it 'Can get weather of a given latitude and longitude location', :vcr do
+    term = 'Mostly Cloudy'
+
+    response = GiphyService.get_gif(term)
+
+    expect(response).to have_key(:data)
+    expect(response[:data][0][:url]).to eq("https://giphy.com/gifs/sky-clouds-cloud-HoUgegTjteXCw")
+  end
+end
