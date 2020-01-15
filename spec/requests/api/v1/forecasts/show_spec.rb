3646d495f484c82860eb1a7fce744a8d166ae301
Michael Cooper <macoop2363@gmail.com>
2020-01-15 10:40:38 -0700

    Add default response if the location is gibberish

--- spec/requests/api/v1/forecasts/show_spec.rb
+++ spec/requests/api/v1/forecasts/show_spec.rb
@@ -29,4 +29,31 @@ describe 'Forecast Show:', type: :request do
       expect(response[:daily].first).to have_key(:precip_type)
     end
   end
+
+  it 'I get a default response (where location=denver,co) if the location was invalid', :vcr do
+    get '/api/v1/forecast?location=asdf'
+
+    response = JSON.parse(@response.body, symbolize_names: true)[:data][:attributes]
+
+    expect(response[:currently]).to have_key(:time)
+    expect(response[:currently]).to have_key(:summary)
+    expect(response[:currently]).to have_key(:icon)
+    expect(response[:currently]).to have_key(:temperature)
+    expect(response[:currently]).to have_key(:apparent_temperature)
+    expect(response[:currently]).to have_key(:humidity)
+    expect(response[:currently]).to have_key(:visibility)
+    expect(response[:currently]).to have_key(:uv_index)
+  
+    expect(response[:hourly].length).to eq(49)
+    expect(response[:hourly].first).to have_key(:time)
+    expect(response[:hourly].first).to have_key(:summary)
+    expect(response[:hourly].first).to have_key(:icon)
+    expect(response[:hourly].first).to have_key(:temperature)
+  
+    expect(response[:daily].length).to eq(8)
+    expect(response[:daily].first).to have_key(:temperature_low)
+    expect(response[:daily].first).to have_key(:temperature_high)
+    expect(response[:daily].first).to have_key(:precip_probability)
+    expect(response[:daily].first).to have_key(:precip_type)
+  end
 end
