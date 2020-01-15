f08b328d52a83d7b9883d5746cf07d5d4b53f74d
Michael Cooper <macoop2363@gmail.com>
2020-01-15 09:57:36 -0700

    Add default image if the user provides an invalid location

--- app/facades/background_facade.rb
+++ app/facades/background_facade.rb
@@ -11,7 +11,19 @@ class BackgroundFacade
   private
 
   def get_url(location)
-    image = ImageService.get_image(location)
-    @url = image[:photos][:results][0][:urls][:full]
+    response = ImageService.get_image(location)
+    has_photos?(response) ? update_url(response) : default_url
+  end
+  
+  def has_photos?(response)
+    !response[:photos][:results].empty?
+  end
+  
+  def update_url(response)
+    @url = response[:photos][:results][0][:urls][:full]
+  end
+
+  def default_url
+    @url = 'https://images.unsplash.com/photo-1494870363241-b5225be3dada?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjExMDg5MX0'
   end
 end
