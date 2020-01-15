3c1c0e33830fe59d5afc4425c7c8c5528c80a805
Michael Cooper <macoop2363@gmail.com>
2020-01-14 17:52:58 -0700

    Change method name to better reflect its response and create additional method to get the weather at a future time

--- app/services/dark_sky_service.rb
+++ app/services/dark_sky_service.rb
@@ -1,18 +1,31 @@
 class DarkSkyService
   DARK_SKY_URL = 'https://api.darksky.net'.freeze
 
-  def self.get_weather(location)
+  def self.get_current_weather(location)
     response = ApiQuerier.query_api(
       DARK_SKY_URL,
-      create_forecast_endpoint(location)
+      create_current_forecast_endpoint(location)
+    )
+  end
+
+  def self.get_future_weather(location, time)
+    response = ApiQuerier.query_api(
+      DARK_SKY_URL,
+      create_future_forecast_endpoint(location, time)
     )
   end
 
   private_class_method
 
-  def self.create_forecast_endpoint(location)
+  def self.create_current_forecast_endpoint(location)
     lat  = location.latitude
     long = location.longitude
     "forecast/#{ENV['DARK_SKY_API_KEY']}/#{lat},#{long}"
+  end
+
+  def self.create_future_forecast_endpoint(location, time)
+    lat  = location.latitude
+    long = location.longitude
+    "forecast/#{ENV['DARK_SKY_API_KEY']}/#{lat},#{long},#{time}"
   end
 end
--- spec/services/dark_sky_service_spec.rb
+++ spec/services/dark_sky_service_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe DarkSkyService do
   it 'Can get weather of a given latitude and longitude location', :vcr do
     location = GeoCoordinate.new({ lat: 39.7392358, lng: -104.990251 })
 
-    response = DarkSkyService.get_weather(location)
+    response = DarkSkyService.get_current_weather(location)
 
     expect(response).to have_key(:currently)
     expect(response).to have_key(:minutely)
@@ -18,5 +18,14 @@ RSpec.describe DarkSkyService do
     expect(response).to have_key(:daily)
     expect(response[:daily]).to have_key(:data)
     expect(response[:daily][:data]).to be_an(Array)
+  end
+
+  it 'Can get the forecast of a given latitude and longitude at a given time', :vcr do
+    location = GeoCoordinate.new({ lat: 39.7392358, lng: -104.990251 })
+
+    response = DarkSkyService.get_future_weather(location, 1579059119)
+
+    expect(response).to have_key(:currently)
+    expect(response[:currently][:time]).to eq(1579059119)
   end
 end
